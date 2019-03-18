0e35674ba45e5b11e7b365031202e2af6f9199be
JHLFS <251630332@qq.com>
2019-03-18 20:20:33 +0800

    修改数据库连接设置

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bb2',
-    'username' => 'bb2',
+    'username' => 'root',
     'password' => '490526',
     'charset' => 'utf8',
 ];
